741825401a69d42dd336fa52c46fb39a251c3537
jasonnlw <jason.nlw@gmail.com>
2025-10-29 15:04:04 +0000

    Add configuration settings for application

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window.CONFIG = {
+  BASE: "https://snarc-llgc.wikibase.cloud",
+  ACTION_API: "https://snarc-llgc.wikibase.cloud/w/api.php",
+  SPARQL: "https://snarc-llgc.wikibase.cloud/query/sparql",
+  DEFAULT_LANG: "en",
+  TYPES: { person: "Q5" }
+};
