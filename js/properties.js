f8c71f9dcac796ae467429d6040468afcde4237b
jasonnlw <jason.nlw@gmail.com>
2025-10-30 09:32:28 +0000

    Add files via upload

---
+++ js/properties.js
@@ -0,0 +1,502 @@
+window.PROPERTY_INFO = {
+  "P10": {
+    "label_en": "VIAF ID",
+    "label_cy": "VIAF ID",
+    "datatype": "ExternalId"
+  },
+  "P100": {
+    "label_en": "monumental inscription",
+    "label_cy": "arysgrif cofadail",
+    "datatype": "String"
+  },
+  "P101": {
+    "label_en": "author",
+    "label_cy": "awdur",
+    "datatype": "WikibaseItem"
+  },
+  "P102": {
+    "label_en": "published works",
+    "label_cy": "gweithiau cyhoeddedig",
+    "datatype": "ExternalId"
+  },
+  "P103": {
+    "label_en": "title",
+    "label_cy": "teitl",
+    "datatype": "String"
+  },
+  "P104": {
+    "label_en": "subject has role",
+    "label_cy": "rôl y pwnc",
+    "datatype": "WikibaseItem"
+  },
+  "P107": {
+    "label_en": "NLW Catalogue ID",
+    "label_cy": "ID Catalog LLGC",
+    "datatype": "ExternalId"
+  },
+  "P108": {
+    "label_en": "CLIP Cymru Content",
+    "label_cy": "Cynnwys CLIP Cymru",
+    "datatype": "ExternalId"
+  },
+  "P11": {
+    "label_en": "Library of Congress authority ID",
+    "label_cy": "Library of Congress authority ID",
+    "datatype": "ExternalId"
+  },
+  "P12": {
+    "label_en": "National Library of Wales Authority ID",
+    "label_cy": "dynodwr Llyfrgell Genedlaethol Cymru",
+    "datatype": "ExternalId"
+  },
+  "P13": {
+    "label_en": "sex or gender",
+    "label_cy": "rhyw",
+    "datatype": "WikibaseItem"
+  },
+  "P14": {
+    "label_en": "nationality",
+    "label_cy": "cenedligrwydd",
+    "datatype": "WikibaseItem"
+  },
+  "P15": {
+    "label_en": "given name",
+    "label_cy": "enw cyntaf",
+    "datatype": "WikibaseItem"
+  },
+  "P16": {
+    "label_en": "family name",
+    "label_cy": "cyfenw",
+    "datatype": "WikibaseItem"
+  },
+  "P17": {
+    "label_en": "date of birth",
+    "label_cy": "dyddiad geni",
+    "datatype": "Time"
+  },
+  "P18": {
+    "label_en": "date of death",
+    "label_cy": "dyddiad marw",
+    "datatype": "Time"
+  },
+  "P19": {
+    "label_en": "short biography",
+    "label_cy": "bywgraffiad byr",
+    "datatype": "String"
+  },
+  "P2": {
+    "label_en": "Wikidata Item ID",
+    "label_cy": "ID yr Eitem ar Wikidata",
+    "datatype": "ExternalId"
+  },
+  "P20": {
+    "label_en": "language spoken or written",
+    "label_cy": "iaith y person",
+    "datatype": "WikibaseItem"
+  },
+  "P21": {
+    "label_en": "place of birth",
+    "label_cy": "man geni",
+    "datatype": "WikibaseItem"
+  },
+  "P22": {
+    "label_en": "place of death",
+    "label_cy": "man marw",
+    "datatype": "WikibaseItem"
+  },
+  "P23": {
+    "label_en": "educated at",
+    "label_cy": "addysgwyd yn",
+    "datatype": "WikibaseItem"
+  },
+  "P24": {
+    "label_en": "pseudonym",
+    "label_cy": "ffugenw",
+    "datatype": "String"
+  },
+  "P25": {
+    "label_en": "occupation",
+    "label_cy": "galwedigaeth",
+    "datatype": "WikibaseItem"
+  },
+  "P26": {
+    "label_en": "coordinate location",
+    "label_cy": "cyfesurynnau'r lleoliad",
+    "datatype": "GlobeCoordinate"
+  },
+  "P27": {
+    "label_en": "located in the administrative territorial entity",
+    "label_cy": "lleolir yn yr ardal weinyddol",
+    "datatype": "WikibaseItem"
+  },
+  "P28": {
+    "label_en": "official name",
+    "label_cy": "enw swyddogol",
+    "datatype": "String"
+  },
+  "P29": {
+    "label_en": "property constraint",
+    "label_cy": "cyfyngiad y nodwedd",
+    "datatype": "WikibaseItem"
+  },
+  "P3": {
+    "label_en": "formatter URL",
+    "label_cy": "fformatiwr URL",
+    "datatype": "String"
+  },
+  "P30": {
+    "label_en": "property",
+    "label_cy": "priodwedd",
+    "datatype": "WikibaseProperty"
+  },
+  "P31": {
+    "label_en": "Wikimedia Commons media",
+    "label_cy": "delwedd Wikimedia Commons",
+    "datatype": "CommonsMedia"
+  },
+  "P32": {
+    "label_en": "described by source",
+    "label_cy": "ffynhonnell y disgrifiad",
+    "datatype": "WikibaseItem"
+  },
+  "P33": {
+    "label_en": "retrieved",
+    "label_cy": "dyddiad cyrchiad",
+    "datatype": "Time"
+  },
+  "P34": {
+    "label_en": "reference URL",
+    "label_cy": "cyfeirnod URL",
+    "datatype": "Url"
+  },
+  "P35": {
+    "label_en": "stated in",
+    "label_cy": "datganwyd yn",
+    "datatype": "WikibaseItem"
+  },
+  "P36": {
+    "label_en": "property scope",
+    "label_cy": "nan",
+    "datatype": "WikibaseItem"
+  },
+  "P37": {
+    "label_en": "language of work or name",
+    "label_cy": "iaith y gwaith neu'r enw",
+    "datatype": "WikibaseItem"
+  },
+  "P38": {
+    "label_en": "historic county",
+    "label_cy": "sir hanesyddol",
+    "datatype": "WikibaseItem"
+  },
+  "P39": {
+    "label_en": "constraint status",
+    "label_cy": "nan",
+    "datatype": "WikibaseItem"
+  },
+  "P4": {
+    "label_en": "Wikidata property",
+    "label_cy": "priodwedd Wikidata",
+    "datatype": "ExternalId"
+  },
+  "P40": {
+    "label_en": "start time",
+    "label_cy": "dyddiad neu amser cychwyn",
+    "datatype": "Time"
+  },
+  "P41": {
+    "label_en": "end time",
+    "label_cy": "dyddiad gorffen neu derfyn",
+    "datatype": "Time"
+  },
+  "P43": {
+    "label_en": "complementary property",
+    "label_cy": "nan",
+    "datatype": "WikibaseProperty"
+  },
+  "P44": {
+    "label_en": "opposite or inverse property",
+    "label_cy": "y gwrthwyneb i",
+    "datatype": "WikibaseProperty"
+  },
+  "P45": {
+    "label_en": "subclass of",
+    "label_cy": "isddosbarth o'r canlynol",
+    "datatype": "WikibaseItem"
+  },
+  "P46": {
+    "label_en": "earliest date",
+    "label_cy": "dyddiad cynharaf",
+    "datatype": "Time"
+  },
+  "P47": {
+    "label_en": "latest date",
+    "label_cy": "dyddiad hwyraf",
+    "datatype": "Time"
+  },
+  "P48": {
+    "label_en": "IIIF manifest",
+    "label_cy": "manifest IIIF",
+    "datatype": "Url"
+  },
+  "P49": {
+    "label_en": "described at URL",
+    "label_cy": "disgrifiwyd yn yr URL",
+    "datatype": "Url"
+  },
+  "P5": {
+    "label_en": "Welsh Biography ID (EN)",
+    "label_cy": "ID Y Bywgraffiadur (EN)",
+    "datatype": "ExternalId"
+  },
+  "P50": {
+    "label_en": "NLW media",
+    "label_cy": "delwedd LLGC",
+    "datatype": "ExternalId"
+  },
+  "P52": {
+    "label_en": "sibling",
+    "label_cy": "brawd neu chwaer",
+    "datatype": "WikibaseItem"
+  },
+  "P53": {
+    "label_en": "father",
+    "label_cy": "tad",
+    "datatype": "WikibaseItem"
+  },
+  "P54": {
+    "label_en": "child",
+    "label_cy": "plentyn",
+    "datatype": "WikibaseItem"
+  },
+  "P55": {
+    "label_en": "mother",
+    "label_cy": "mam",
+    "datatype": "WikibaseItem"
+  },
+  "P56": {
+    "label_en": "spouse",
+    "label_cy": "priod",
+    "datatype": "WikibaseItem"
+  },
+  "P57": {
+    "label_en": "unmarried partner",
+    "label_cy": "cymar",
+    "datatype": "WikibaseItem"
+  },
+  "P58": {
+    "label_en": "step parent",
+    "label_cy": "llys-riant",
+    "datatype": "WikibaseItem"
+  },
+  "P59": {
+    "label_en": "relative",
+    "label_cy": "perthynas (teulu)",
+    "datatype": "WikibaseItem"
+  },
+  "P6": {
+    "label_en": "Welsh Biography ID (CY)",
+    "label_cy": "ID Y Bywgraffiadur (CY)",
+    "datatype": "ExternalId"
+  },
+  "P60": {
+    "label_en": "date christened",
+    "label_cy": "dyddiad bedydd",
+    "datatype": "Time"
+  },
+  "P61": {
+    "label_en": "date of burial or cremation",
+    "label_cy": "dyddiad claddu neu amlosgi",
+    "datatype": "Time"
+  },
+  "P62": {
+    "label_en": "wikidata URI",
+    "label_cy": "URI Wikidata",
+    "datatype": "Url"
+  },
+  "P63": {
+    "label_en": "country",
+    "label_cy": "gwladwriaeth",
+    "datatype": "WikibaseItem"
+  },
+  "P64": {
+    "label_en": "part of",
+    "label_cy": "rhan o'r canlynol",
+    "datatype": "WikibaseItem"
+  },
+  "P65": {
+    "label_en": "has part(s)",
+    "label_cy": "yn cynnwys",
+    "datatype": "WikibaseItem"
+  },
+  "P66": {
+    "label_en": "capital city",
+    "label_cy": "prifddinas",
+    "datatype": "WikibaseItem"
+  },
+  "P67": {
+    "label_en": "accuracy",
+    "label_cy": "cywirdeb",
+    "datatype": "WikibaseItem"
+  },
+  "P68": {
+    "label_en": "CADW Building ID",
+    "label_cy": "dynodwr Cadw (adeilad)",
+    "datatype": "ExternalId"
+  },
+  "P69": {
+    "label_en": "Coflein ID",
+    "label_cy": "dynodwr Coflein",
+    "datatype": "ExternalId"
+  },
+  "P7": {
+    "label_en": "instance of",
+    "label_cy": "enghraifft o'r canlynol",
+    "datatype": "WikibaseItem"
+  },
+  "P70": {
+    "label_en": "diocese",
+    "label_cy": "esgobaeth",
+    "datatype": "WikibaseItem"
+  },
+  "P71": {
+    "label_en": "parish church",
+    "label_cy": "eglwys blwyf",
+    "datatype": "WikibaseItem"
+  },
+  "P72": {
+    "label_en": "source of name form",
+    "label_cy": "ffynhonnell ffurf yr enw",
+    "datatype": "WikibaseItem"
+  },
+  "P73": {
+    "label_en": "location",
+    "label_cy": "lleoliad",
+    "datatype": "WikibaseItem"
+  },
+  "P75": {
+    "label_en": "NLW authority id test",
+    "label_cy": "nan",
+    "datatype": "String"
+  },
+  "P76": {
+    "label_en": "owner of",
+    "label_cy": "perchennog y canlynol",
+    "datatype": "WikibaseItem"
+  },
+  "P77": {
+    "label_en": "owned by",
+    "label_cy": "perchennog",
+    "datatype": "WikibaseItem"
+  },
+  "P78": {
+    "label_en": "residence",
+    "label_cy": "man preswyl",
+    "datatype": "WikibaseItem"
+  },
+  "P79": {
+    "label_en": "occupant",
+    "label_cy": "deiliad",
+    "datatype": "WikibaseItem"
+  },
+  "P8": {
+    "label_en": "ISO 639-1 code",
+    "label_cy": "côd ISO 639-1",
+    "datatype": "ExternalId"
+  },
+  "P80": {
+    "label_en": "work title",
+    "label_cy": "teitl y gwaith",
+    "datatype": "String"
+  },
+  "P81": {
+    "label_en": "social classification",
+    "label_cy": "dosbarth cymdeithasol",
+    "datatype": "WikibaseItem"
+  },
+  "P82": {
+    "label_en": "Authority Source",
+    "label_cy": "Ffynhonnell yr Awdurdod",
+    "datatype": "WikibaseItem"
+  },
+  "P83": {
+    "label_en": "Historic Place Names of Wales ID",
+    "label_cy": "dynodwr Enwau Lleoedd Hanesyddol Cymru",
+    "datatype": "ExternalId"
+  },
+  "P84": {
+    "label_en": "Cadw Monument ID",
+    "label_cy": "dynodwr Cadw (heneb)",
+    "datatype": "ExternalId"
+  },
+  "P85": {
+    "label_en": "date of publication",
+    "label_cy": "dyddiad cyhoeddi",
+    "datatype": "Time"
+  },
+  "P86": {
+    "label_en": "on focus list for NLW project",
+    "label_cy": "ar restr sylw'r prosiect LLGC",
+    "datatype": "WikibaseItem"
+  },
+  "P87": {
+    "label_en": "inception",
+    "label_cy": "dechreuwyd",
+    "datatype": "Time"
+  },
+  "P88": {
+    "label_en": "headquarters location",
+    "label_cy": "lleoliad pencadlys",
+    "datatype": "WikibaseItem"
+  },
+  "P89": {
+    "label_en": "founded by",
+    "label_cy": "sylfaenydd",
+    "datatype": "WikibaseItem"
+  },
+  "P9": {
+    "label_en": "ISNI ID",
+    "label_cy": "ISNI ID",
+    "datatype": "ExternalId"
+  },
+  "P90": {
+    "label_en": "Repertory of Welsh Manuscripts: Place ID",
+    "label_cy": "Repertory o Lawysgrifau Cymreig: ID lleoedd",
+    "datatype": "String"
+  },
+  "P91": {
+    "label_en": "Standardised Welsh Place-names ID",
+    "label_cy": "ID Enwau Lleoedd Safonol Cymru",
+    "datatype": "ExternalId"
+  },
+  "P93": {
+    "label_en": "point in time",
+    "label_cy": "pwynt mewn amser",
+    "datatype": "Time"
+  },
+  "P95": {
+    "label_en": "dissolved, abolished or demolished date",
+    "label_cy": "daeth i ben",
+    "datatype": "Time"
+  },
+  "P96": {
+    "label_en": "affiliation",
+    "label_cy": "tadogaeth",
+    "datatype": "WikibaseItem"
+  },
+  "P97": {
+    "label_en": "UK National Archives ID",
+    "label_cy": "dynodwr Archifau Cenedlaethol y DU",
+    "datatype": "ExternalId"
+  },
+  "P98": {
+    "label_en": "National Library of Wales Collection Identifier",
+    "label_cy": "Dynodwr Casgliad Llyfrgell Genedlaethol Cymru",
+    "datatype": "String"
+  },
+  "P99": {
+    "label_en": "collection",
+    "label_cy": "casgliad",
+    "datatype": "WikibaseItem"
+  }
+};
