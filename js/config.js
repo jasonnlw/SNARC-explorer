7b085c59b513a829398b6b3a9c8ee0f481e491e2
jasonnlw <jason.nlw@gmail.com>
2025-11-10 20:45:37 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,6 @@
 window.CONFIG = {
   BASE: "https://snarc-llgc.wikibase.cloud",
   ACTION_API: "https://snarc-llgc.wikibase.cloud/w/api.php",
-  SPARQL: "https://snarc-llgc.wikibase.cloud/query/sparql",
   DEFAULT_LANG: "en",
 
   // Property IDs used for filtering
