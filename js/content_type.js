5ae29b72042855ef3352e3820b4dc401fa6ed005
jasonnlw <jason.nlw@gmail.com>
2025-12-04 10:15:13 +0000

    Create content_type.js

---
+++ js/content_type.js
@@ -0,0 +1,8 @@
+[
+{ "id": "P12", "label_en": "Archives", "label_cy": "Archifau" },
+{ "id": "P50", "label_en": "Images", "label_cy": "Delweddau" },
+{ "id": "P102", "label_en": "Printed Collections", "label_cy": "Casgliadau Print" },
+{ "id": "P108", "label_en": "Screen and Sound", "label_cy": "Sgrin a Sain" },
+{ "id": "P5", "label_en": "Welsh Biography (English)", "label_cy": "Bywgraffiadur Cymraeg (Saesneg)" },
+{ "id": "P6", "label_en": "Welsh Biography (Welsh)", "label_cy": "Bywgraffiadur Cymraeg (Cymraeg)" }
+  ]
